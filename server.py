8f6b7587e343b7432c529a276f8c06150a91c2ab
Nerroth <nerroth@flappycurve.com>
2022-08-08 19:03:43 +0200

    Implement game SWF version switching on login page

--- server.py
+++ server.py
@@ -39,10 +39,13 @@ print (" [+] Configuring server routes...")
 def login():
     # Log out previous session
     session.pop('USERID', default=None)
+    session.pop('GAMEVERSION', default=None)
     # If logging in, set session USERID, and go to play
     if request.method == 'POST':
         session['USERID'] = request.form['USERID']
+        session['GAMEVERSION'] = request.form['GAMEVERSION']
         print("[LOGIN] USERID:", request.form['USERID'])
+        print("[LOGIN] GAMEVERSION:", request.form['GAMEVERSION'])
         return redirect("/play.html")
     # Login page
     if request.method == 'GET':
@@ -51,19 +54,26 @@ def login():
 
 @app.route("/play.html")
 def play():
+    print(session)
+
     if 'USERID' not in session:
+        return redirect("/")
+    if 'GAMEVERSION' not in session:
         return redirect("/")
 
     if session['USERID'] not in all_saves_userid():
         return redirect("/")
     
     USERID = session['USERID']
+    GAMEVERSION = session['GAMEVERSION']
     print("[PLAY] USERID:", USERID)
-    return render_template("play.html", save_info=save_info(USERID), serverTime=timestamp_now(), version=version_name)
+    print("[PLAY] GAMEVERSION:", GAMEVERSION)
+    return render_template("play.html", save_info=save_info(USERID), serverTime=timestamp_now(), version=version_name, GAMEVERSION=GAMEVERSION)
 
 @app.route("/new.html")
 def new():
     session['USERID'] = new_village()
+    session['GAMEVERSION'] = "SocialEmpires0926bsec.swf"
     return redirect("play.html")
 
 @app.route("/crossdomain.xml")
