f13153f97d42114e04a2cd9055ca3e9ff601adf8
AcidCaos <acidcaos1@gmail.com>
2022-08-13 12:40:29 +0200

    Temp fix: migration for dev versions between builds

--- version.py
+++ version.py
@@ -10,23 +10,29 @@ def migrate_loaded_save(save: dict) -> bool:
     if "version" not in save:
         save["version"] = "0.01a"
     
-    if save["version"] == version_code: # save is from the current version
-        return False
+    # if save["version"] == version_code: # save is from the current version
+    #     return False
     
     # 0.01a -> 0.02a
     if save["version"] == "0.01a":
         save["maps"][0]["timestamp"] = timestamp_now()
         save["privateState"]["dartsRandomSeed"] = abs(int((2**16 - 1) * random.random()))
         save["version"] = "0.02a"
-
         print("   > migrated to 0.02a")
     
-    # # 0.02a -> 0.03a
-    # if save["version"] == "0.02a":
-    #     save["version"] = "0.03a"
-    #     save["privateState"]["arrayAnimals"] = {} # fix no animal spawning
-    #     save["privateState"]["strategy"] = 8 # fix crash when attacking a player
-    #     save["maps"][0]["universAttackWin"] = [] # pvp current island progress
-    #     print("   > migrated to 0.03a")
+    # 0.02a -> 0.03a
+    if save["version"] == "0.02a":
+        if "arrayAnimals" not in save["privateState"]:
+            save["privateState"]["arrayAnimals"] = {} # fix no animal spawning
+        if "strategy" not in save["privateState"]:
+            save["privateState"]["strategy"] = 8 # fix crash when attacking a player
+        if "universAttackWin" not in save["maps"][0]:
+            save["maps"][0]["universAttackWin"] = [] # pvp current island progress
+        if "questTimes" not in save["maps"][0]:
+            save["maps"][0]["questTimes"] = [] # quests
+        if "lastQuestTimes" not in save["maps"][0]:
+            save["maps"][0]["lastQuestTimes"] = [] # 1.1.5 quests
+        # save["version"] = "0.03a"
+        print("   > migrated to 0.03a")
 
     return True
