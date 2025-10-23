65a3d71579ace4989a5d090bc3041ad0e602d349
Nerroth <nerroth@flappycurve.com>
2025-10-23 17:36:37 +0200

    Bind start_quest_new to cmd_nop as it sends no data

--- command.py
+++ command.py
@@ -148,6 +148,7 @@ commands = {
 	# quests, tournament --------------------------------------------------------------------------------------
 	"set_attack_team":					cmd_set_attack_team,
 	"start_quest":						cmd_start_quest,
+	"start_quest_new":					cmd_nop,
 	"end_quest":						cmd_end_quest,
 	# pvp -----------------------------------------------------------------------------------------------------
 	"buy_shield":						cmd_buy_shield,
