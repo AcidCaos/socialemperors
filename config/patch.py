b4912be893cd6db0d15fa33cf1398a0f49bb3987
Nerroth <nerroth@flappycurve.com>
2025-10-26 19:54:29 +0100

    Remove unecessary tool from config/ that was used to make a mod
    A mod that no longer works

--- config/patch.py
+++
@@ -1,9 +0,0 @@
-import json
-import jsonpatch
-
-original = json.load(open("./config/game_config_20120826.json", 'r'))
-patched = json.load(open("./config/patched_config.json", 'r'))
-
-patch = jsonpatch.make_patch(original, patched)
-
-print(str(patch))
