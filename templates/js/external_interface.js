d8011caecac2310b06d146e811ceae86cd903c2b
Nerroth <nerroth@flappycurve.com>
2025-10-28 13:45:27 +0100

    Improve ExternalInterface calls and display sent data in console log
    Should help with figuring out how this works

--- templates/js/external_interface.js
+++ templates/js/external_interface.js
@@ -1,16 +1,67 @@
 // PVP Ranking
-function showLeaderboard(param)
+function showLeaderboard(...param)
 {
-	if (param[0] === "list-pt")
+	if (param[0][0] === "list-pt")
 	{
 		window.open("/pvp/ranking", '_blank').focus();
 	}
 }
 // Graveyard Ask for Potions
-function showPopupSEHelpPotion(param)
+function showPopupSEHelpPotion(...param)
 {
-	if (param.length == 0)
+	if (param[0].length == 0)
 	{
 		window.open("/graveyard/potions", '_blank').focus();
 	}
 }
+// ??
+function sendFbMessage(...param)
+{
+	// player_uid
+	_ei_debug("sendFbMessage", param);
+}
+// Share Lost Item
+function showFeedItemFound(...param)
+{
+	// voluntary, tipo, id
+	_ei_debug("showFeedItemFound", param);
+}
+// Events -> Hire Friends
+function showPopupSEHelpOffer(...param)
+{
+	// event_id
+	_ei_debug("showPopupSEHelpOffer", param);
+}
+// SI Building Hiring
+function showPopupSEHelpSocialItem(...param)
+{
+	// bx, by, town_id, b_id, si, b_name
+	_ei_debug("showPopupSEHelpSocialItem", param);
+}
+// ??
+function showFeed(...param)
+{
+	// Land Expansion
+	// confirm:
+	// true, "expansion", ownedGrid.length - 2, ownerGrid.length, ""
+	// deny:
+	// false, "expansion", ownedGrid.length - 2, ownerGrid.length, ""
+	_ei_debug("showPopupSEHelpNest", param);
+	
+}
+// Dragon Nest Help
+function showPopupSEHelpNest(...param)
+{
+	// all_friends, nest_name
+	// use privateState.dragonSocialHelp as SI
+	// client sends Constants.CMD_CLEAR_DRAGON_HIRES command with SI data
+	_ei_debug("showPopupSEHelpNest", param);
+}
+
+// Debug
+function _ei_debug(func_name, ...param)
+{
+	console.log("ExternalInterface: " + func_name + " -> " + JSON.stringify(param[0], null, '\t'));
+}
+
+console.log("ExternalInterface initialized!");
